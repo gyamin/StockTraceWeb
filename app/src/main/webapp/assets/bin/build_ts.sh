e8a932f440a8095304deeb00ae49aa79d5b92f73
Yasumasa Nogami <n.gyamin@gmail.com>
2016-10-14 11:16:45 +0900

    AngularJS TutorialでAngularの基本を理解する Tutorial 6 #2

--- app/src/main/webapp/assets/bin/build_ts.sh
+++ app/src/main/webapp/assets/bin/build_ts.sh
@@ -5,8 +5,8 @@ source ${execDir}/_common.sh
 # compile typescript files
 if [ "${1}" = "watch" ]; then
     tsc -w --rootDir ${jsSrcDir} --outDir ${jsOutDir} &
-    ${baseDir}/node_modules/watch/cli.js "rsync -av --delete --include='*/' --include='*.html' --exclude='*' ${jsSrcDir}/ ${jsOutDir}/" ${jsSrcDir}
+    ${baseDir}/node_modules/watch/cli.js "rsync -av --delete --include='*/' --include='*.html' --include='*.json' --exclude='*' ${jsSrcDir}/ ${jsOutDir}/" ${jsSrcDir}
 else
     tsc --rootDir ${jsSrcDir} --outDir ${jsOutDir}
-    rsync -av --delete --include='*/' --include='*.html' --exclude='*' ${jsSrcDir}/ ${jsOutDir}/
+    rsync -av --delete --include='*/' --include='*.html' --include='*.json' --exclude='*' ${jsSrcDir}/ ${jsOutDir}/
 fi
--- app/src/main/webapp/assets/js/tutorial/phone-list/phone-list.component.ts
+++ app/src/main/webapp/assets/js/tutorial/phone-list/phone-list.component.ts
@@ -3,20 +3,11 @@ angular.
 module('phoneList').
 component('phoneList', {
     templateUrl: './../../js/tutorial/phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-        this.phones = [
-            {
-                name: 'Nexus S A',
-                snippet: 'Fast just got faster with Nexus S.'
-            }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
-            }
-        ];
-
-        // this.orderProp = 'age';
+    controller: function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+        $http.get('./../../js/tutorial/phones/phones.json').then(function(response) {
+            self.phones = response.data;
+        })
     }
 });
