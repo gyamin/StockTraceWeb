1bb7316e96d383f86a9e3fe04013e07b5c6e77c9
Yasumasa Nogami <n.gyamin@gmail.com>
2016-12-08 21:54:08 +0900

    JdbcTemplateを利用したDBアクセスをテスト
    test/java/com/gyamin/stocktrace/dao/StockPricesDaoJdbcTemplate.javaからdaoを呼び出し、DBアクセスできることを確認
    #6

--- app/src/main/java/com/gyamin/stocktrace/controller/Stock.java
+++ app/src/main/java/com/gyamin/stocktrace/controller/Stock.java
@@ -1,8 +1,7 @@
 package com.gyamin.stocktrace.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
-import com.gyamin.stocktrace.bean.StockFluctuationInPeriodBean;
-import com.gyamin.stocktrace.entity.StockPrices;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import com.gyamin.stocktrace.service.StockSearch;
-
-import java.util.List;
 
 @Controller
 public class Stock {
@@ -24,13 +21,13 @@ public class Stock {
         return "stocks/index";
     }
 
-    @RequestMapping(value = "/stocks/search", method = RequestMethod.GET)
-    public String search(
-            @RequestParam(defaultValue = "東証一部") String section,
-            @RequestParam String issueCode,
-            Model model) {
-        StockPrices stockPrices = stockSearch.selectById();
-        model.addAttribute("modelPrices", stockPrices);
-        return "stocks/search";
-    }
+//    @RequestMapping(value = "/stocks/search", method = RequestMethod.GET)
+//    public String search(
+//            @RequestParam(defaultValue = "東証一部") String section,
+//            @RequestParam String issueCode,
+//            Model model) {
+//        List<StockPrices> stockPrices = stockSearch.();
+//        model.addAttribute("modelPrices", stockPrices);
+//        return "stocks/search";
+//    }
 }
--- app/src/main/java/com/gyamin/stocktrace/service/StockSearch.java
+++ app/src/main/java/com/gyamin/stocktrace/service/StockSearch.java
@@ -1,32 +1,27 @@
 package com.gyamin.stocktrace.service;
 
 import com.gyamin.stocktrace.dao.StockPricesDaoImpl;
-import com.gyamin.stocktrace.entity.StockPrices;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
-import com.gyamin.stocktrace.bean.StockFluctuationInPeriodBean;
 
 import com.gyamin.stocktrace.dao.StockPricesDao;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public class StockSearch {
 
     private final StockPricesDao dao = new StockPricesDaoImpl();
 
-    public StockPrices selectById() {
-
+//    public StockPrices selectByPrimaryKey() {
+//
 //        // データベース設定を取得
 //        ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-postgre.xml");
 //        // bean設定を取得しインスタンス作成
 //        StockPricesDao stockPricesDao = ctx.getBean(StockPricesDao.class);
-
-        // stockPricesDaoを利用してDBアクセス、株価価格情報を取得
-        StockPrices stockPrices = dao.selectById(LocalDate.of(2015, 11, 13), 1333);
-
-        return stockPrices;
-    }
+//
+//        // stockPricesDaoを利用してDBアクセス、株価価格情報を取得
+//        StockPrices stockPrices = dao.selectById(LocalDate.of(2015, 11, 13), 1333);
+//
+//        return stockPrices;
+//    }
 }
